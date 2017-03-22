236fa0e2d0c62e58842c9e572112b2dd65668b1d
Emmanuel ESCOLA <emmanuel.escola@soprasteria.com>
2017-03-22 10:58:32 +0100

    Add select for qualities and armors

---
+++ data/qualities.js
@@ -0,0 +1,8 @@
+Data.set('qualities', Quality, [
+	{id: 1, name: "Mauvaise", bonus:"-1D CAC ou Tir"},
+    {id: 2, name: "Normale", bonus:"Aucun Bonus/Malus"},
+    {id: 3, name: "Supérieure", bonus:"+1 CAC ou Tir"},
+    {id: 4, name: "Extraordinaire", bonus:"+1 CAC ou Tir + 1 Dégâts"},
+    {id: 5, name: "Obsidienne", bonus:"(Dagues et Flèches) Perforante+1 - Fragile"},
+    {id: 6, name: "Os-Dragon", bonus:"Perforante+1 Dégâts +1"}
+]);
---
+++ js/model/armor.js
@@ -0,0 +1,8 @@
+function Armor(data) {
+	AbstractModel.call(this, data)
+}
+
+Armor.create = AbstractModel.create;
+Armor.prototype = {
+	// Any calc method
+};
