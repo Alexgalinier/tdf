48ab8fb6af93c012cf4403e2b9ecfb3ab593cfa0
Alex Galinier <agalinier@sicpa.com>
2017-03-23 08:27:44 +0100

    Refactor structure and select feeding

---
+++ js/index-to-refactor.js
@@ -0,0 +1,85 @@
+$("td").each(function (index, cell) {
+	if(cell.className !== 'td-not-editable') {
+		$(cell).attr('contenteditable', 'true')
+	}
+});
+
+$('#switch-blason').click(function () {
+	$('.sheet').toggleClass("no-bg");
+
+	if ($(this).val() == "Cacher Blason") {
+		$(this).val("Afficher Blason")
+	} else {
+		$(this).val("Cacher Blason")
+	}
+});
+
+$("#vig").on("keyup", updateDefIntrig);
+$("#ing").on("keyup", updateDefIntrig);
+$("#status").on("keyup", updateDefIntrig);
+
+$("#will").on("keyup", updateColdBlood);
+
+$("#agility").on("keyup", updateDefCombat);
+$("#athl").on("keyup", updateDefCombat);
+$("#vig").on("keyup", updateDefCombat);
+$("#malus-armure").on("keyup", updateDefCombat);
+
+$("#end").on("keyup", updateSante);
+
+function updateDefIntrig() {
+	$("#def-intrigue").html(
+		v("vig") + v("ing") + v("status")
+	);
+}
+
+function updateColdBlood() {
+	$("#cold-blood").html(
+		v("will") * 3
+	);
+}
+
+function updateDefCombat() {
+	$('#def-combat').html(
+		v("agility") + v("athl") + v("vig")
+		- toInt($("#malus-armure").html().replace("&nbsp;", ""))
+	)
+}
+
+function updateSante() {
+	var sante = v("end") * 3;
+
+	$('#sante').html(sante);
+
+	$("#damage").find("input").each(function(index) {
+		$(this).prop('checked', false);
+		if (index >= sante)
+			$(this).prop('checked', true);
+	});
+
+	$("#wounds").find("input").each(function(index) {
+		$(this).prop('checked', false);
+		if (index >= v("end")) {
+			$(this).prop('checked', true);
+		}
+	});
+
+	$("#injuries").find("input").each(function(index) {
+		$(this).prop('checked', false);
+		if (index >= v("end")) {
+			$(this).prop('checked', true);
+		}
+	})
+}
+
+// -------------------
+// Private functions
+// -------------------
+function toInt(val) {
+	var int = parseInt(val.trim(), 10)
+	return (isNaN(int)) ? 0 : int
+}
+
+function v(id) {
+	return toInt($("#" + id).val())
+}
